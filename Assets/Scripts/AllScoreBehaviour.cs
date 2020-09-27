00ef5019922630c678e718ce9f35b6213216aa7c
Rattysed <ckvortcov71@gmail.com>
2020-09-27 21:17:31 +0300

    Подправил гитигнор
    А так же немного изменил сцену основную, БУДЬТЕ ОСТОРОЖНЫ!!!!!
    ну и начал рефакторить код

---
+++ Assets/Scripts/AllScoreBehaviour.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class AllScoreBehaviour : MonoBehaviour
+{
+    float resourseMoney;
+    float resourseElectricity;
+    int resoursePeople;
+    [Header("Labels")]
+    public Text labelResourseMoney;
+    public Text labelResourseElectricity;
+    public Text labelResoursePeople;
+    // Start is called before the first frame update
+
+    [System.Serializable]
+    struct GameData
+    {
+        public float money;
+        public float electricity;
+        public int people;
+        public GameData(float money, float electricity,  int people)
+        {
+           
+            this.money = money;
+            this.people = people;
+            this.electricity = electricity;
+            
+        }
+    }
+    void resourseLabelUpdate() // Обновление табличек с числом ресурсов
+    { 
+        labelResourseElectricity.text = Mathf.Floor(resourseElectricity).ToString();
+        labelResourseMoney.text = Mathf.Floor(resourseMoney).ToString();
+        labelResoursePeople.text = resoursePeople.ToString();
+
+    }
+    void LoadFile() // Функция загрузки данных из файла с сохранкой. Можем сделать шифрование, но нахера
+    {
+        string destination = Application.persistentDataPath + "/save.dat";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenRead(destination);
+        else
+        {
+            return;
+        }
+        BinaryFormatter bf = new BinaryFormatter();
+        GameData data = (GameData)bf.Deserialize(file);
+        file.Close();
+        resourseMoney = data.money;
+        resourseElectricity = data.electricity;
+        resoursePeople = data.people;
+        // 
+        // smth = data.smth
+        // *место для других данных*
+    }
+
+    void SaveFile(){
+        string destination = Application.persistentDataPath + "/save.dat";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenWrite(destination);
+        else file = File.Create(destination);
+
+        GameData data = new GameData(resourseMoney, resourseElectricity, resoursePeople /*, Что-то ещё,*/);
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    private void OnApplicationPause(bool pauseStatus) {
+        SaveFile();
+    }
+    void Start()
+    {
+    
+     
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        resourseLabelUpdate();
+    }
+
+}
