071ab5974be5c9e17e6fcc612f2712602799f483
Rattysed <46933547+Rattysed@users.noreply.github.com>
2020-09-29 20:02:58 +0300

    Коммит из Кванта
    Ничего почти не изменилось, закинуты скрипты Вуфории

---
+++ Assets/Scripts/VUFORIA/SideLoadIT.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using Vuforia;
+
+public class SideLoadIT : MonoBehaviour
+{
+    void Start()
+    {
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(CreateImageTargetFromSideloadedTexture);
+    }
+
+    void CreateImageTargetFromSideloadedTexture()
+    {
+        var objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+
+        // get the runtime image source and set the texture to load
+        var runtimeImageSource = objectTracker.RuntimeImageSource;
+        runtimeImageSource.SetFile(VuforiaUnity.StorageType.STORAGE_APPRESOURCE, "Vuforia/myTarget.jpg", 0.15f, "myTargetName");
+
+        // create a new dataset and use the source to create a new trackable
+        var dataset = objectTracker.CreateDataSet();
+        var trackableBehaviour = dataset.CreateTrackable(runtimeImageSource, "myTargetName");
+
+        // add the DefaultTrackableEventHandler to the newly created game object
+        trackableBehaviour.gameObject.AddComponent<DefaultTrackableEventHandler>();
+
+        // activate the dataset
+        objectTracker.ActivateDataSet(dataset);
+
+        // TODO: add virtual content as child object(s)
+    }
+}
